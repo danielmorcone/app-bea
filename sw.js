a1eee8b3afc45036f0191bc87a9eef6869c06a32
danielmorcone <daniel.morcone@gmail.com>
2026-05-26 14:18:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,29 @@
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open('app-v2').then(cache => cache.addAll([
-            '/',
-            '/index.html',
-            '/style.css',
-            '/wave1.png',
-            '/wave2.png',
-            '/wave3.png',
+        caches.open('app-v3').then(cache => cache.addAll([
+            '/app-bea/',
+            '/app-bea/index.html',
+            '/app-bea/calma.html',
+            '/app-bea/style.css',
+            '/app-bea/script.js',
+            '/app-bea/sw.js',
+            '/app-bea/manifest.json',
+            '/app-bea/wave1.png',
+            '/app-bea/wave2.png',
+            '/app-bea/wave3.png',
+            '/app-bea/line.png',
+            '/app-bea/heart.png',
+            '/app-bea/icona.png',
+            '/app-bea/icona-512.png',
         ]))
+    );
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== 'app-v3').map(k => caches.delete(k)))
+        )
     );
 });
 
@@ -16,17 +32,3 @@ self.addEventListener('fetch', e => {
         caches.match(e.request).then(r => r || fetch(e.request))
     );
 });
-
-caches.open('app-v2').then(cache => cache.addAll([
-    '/app-bea/',
-    '/app-bea/index.html',
-    '/app-bea/calma.html',
-    '/app-bea/style.css',
-    '/app-bea/script.js',
-    '/app-bea/wave1.png',
-    '/app-bea/wave2.png',
-    '/app-bea/wave3.png',
-    '/app-bea/line.png',
-    '/app-bea/cuore.png',
-    '/app-bea/manifest.json',
-]))
