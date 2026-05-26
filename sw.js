ea9f73900129d2a25451381e8558b58fc74c9045
danielmorcone <daniel.morcone@gmail.com>
2026-05-26 14:05:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
     e.waitUntil(
-        caches.open('app-v1').then(cache => cache.addAll([
+        caches.open('app-v2').then(cache => cache.addAll([
             '/',
             '/index.html',
             '/style.css',
